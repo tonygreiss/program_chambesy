46a385a83072ec2b5471aae818ab30512caab930
Anthony Greiss <anthonygreiss@anthonys-macbook-air-2.home>
2024-12-08 17:57:03 +0100

    fixing docker build failed on railway.app again

--- backend/run.py
+++ backend/run.py
@@ -6,4 +6,5 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     host = os.environ.get('HOST', '0.0.0.0')
-    app.run(host=host, port=port) 
+    debug = os.environ.get('DEBUG', 'False').lower() == 'true'
+    app.run(host=host, port=port, debug=debug) 
