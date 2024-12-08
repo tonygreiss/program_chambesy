80dde4fb03809ce5de1df972437c0d4e75cc0c57
Anthony Greiss <anthonygreiss@anthonys-macbook-air-2.home>
2024-12-08 18:03:25 +0100

    changed configuration to only deploy backend in railway

--- backend/run.py
+++ backend/run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
     host = os.environ.get('HOST', '0.0.0.0')
     debug = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run(host=host, port=port, debug=debug) 
+    app.run(host=host, port=port, debug=debug)
