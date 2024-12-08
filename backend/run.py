cf18eca7cbc2da9bf147ba74854151d6fc0956db
Anthony Greiss <anthonygreiss@anthonys-macbook-air-2.home>
2024-12-08 18:11:28 +0100

    changes to deply backend

--- backend/run.py
+++ backend/run.py
@@ -5,6 +5,4 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    host = os.environ.get('HOST', '0.0.0.0')
-    debug = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run(host=host, port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port)
