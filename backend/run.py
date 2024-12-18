732364dc962d5a41e0afd38f8773984028d66c76
Anthony Greiss <anthonygreiss@anthonys-macbook-air.home>
2024-12-18 22:33:45 +0100

    Configure backend for Railway deployment

--- backend/run.py
+++ backend/run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port) 
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port) 
